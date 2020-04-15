713c6af982838bb8b310a4d1014283a35710742a
RishivikramN <rishi.vikram.1@gmail.com>
2020-04-15 08:56:34 +0530

    test disabling mongodb logging

--- initializers/loginitializer.js
+++ initializers/loginitializer.js
@@ -5,7 +5,7 @@ const config = require('config');
 
 module.exports = function(){
     winston.add(new winston.transports.File({ filename:'logfile.log', level: 'error' }));
-    winston.add(new winston.transports.MongoDB({ db: config.get('db'), options:{ useUnifiedTopology: true },level: 'error'}));
+    //winston.add(new winston.transports.MongoDB({ db: config.get('db'), options:{ useUnifiedTopology: true },level: 'error'}));
     winston.add(new winston.transports.Console({format: winston.format.simple(), level: 'info'}));
 
     winston.exceptions.handle(
