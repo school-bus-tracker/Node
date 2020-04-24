d7a4875e24f3ebbafbe453c8b25cab7faccefc2c
RishivikramN <rishi.vikram.1@gmail.com>
2020-04-24 17:07:06 +0530

    Added CORS middleware

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const winston = require('winston');
+const express = require("express");
+const winston = require("winston");
+const cors = require("cors");
 
 const app = express();
 
-require('./initializers/loginitializer')();
-require('./initializers/configinitializer')();
-require('./initializers/superusersinitializer')();
-require('./initializers/routesinitializer')(app);
-require('./initializers/dbinitializer')();
-require('./initializers/productioninitializer')(app);
+app.use(cors());
+
+require("./initializers/loginitializer")();
+require("./initializers/configinitializer")();
+require("./initializers/superusersinitializer")();
+require("./initializers/routesinitializer")(app);
+require("./initializers/dbinitializer")();
+require("./initializers/productioninitializer")(app);
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>winston.info(`Listening at port ${port}`));
+app.listen(port, () => winston.info(`Listening at port ${port}`));
